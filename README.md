10bdb23944bc53e4f7e17a4d9d4147242d83ce79
sayampy <81681676+sayampy@users.noreply.github.com>
2021-05-05 11:47:56 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Password-Cracker
+### Password Cracker 
+This will crack all salted hash passwords.
+
+I make this as light weight you can use hardcoded 
+password wordlist that contains all commonly used passwords.
+I convert the password on salted hash then i process it.
+_if you want to test on salted hash without say the salt still it work same if the password in the pass list._
+
+__# This is only for educational purposes.__
---
+++ crack.py
@@ -0,0 +1,34 @@
+import crypt
+
+def crackpass(crypted_pass):
+   flag = 0
+   file = open('pass.txt','r')
+   if crypted_pass == None:
+      print('You didn\'t give the password')
+      return
+   salt = crypted_pass[0:2]
+   for dic in file.readlines():
+       word = dic.strip()
+       cryptWord = crypt.crypt(word,salt = salt)
+       if cryptWord == crypted_pass:
+           print(f'[+] Password Found: {word}\nHash: {cryptWord}')
+           flag+=10
+           break
+   if flag == 0:
+        print('[-] Password Not Found:',crypted_pass)
+
+print("how you want to test/use me")
+mode = input('1. pre hashed passwords or\n2. letter based passwords\n(1/2) ')
+while True:
+   try:
+     if "2" in mode:
+        s = input('Password: ')
+        slt = input('Salt: ')
+        crypted = crypt.crypt(s,salt = slt)
+     elif "1" in mode:
+        crypted = input("Hashed password: ")
+     else:
+       break
+     crackpass(crypted)
+   except (KeyboardInterrupt, EOFError):
+     break
